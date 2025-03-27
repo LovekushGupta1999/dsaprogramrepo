d559c1d0271ce4a8f91ad435738eddec9618f6ba
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-03-27 21:24:47 +0530

    threesum

--- binary tree implementation.cpp
+++ binary tree implementation.cpp
@@ -15,7 +15,7 @@ int main(){
     node *root=new node(10);
     root->left=new node(20);
     root->right=new node(30);
-    cout<<"preorder\n"
+    cout<<"preorder\n";
     cout<<root->data<<endl;
     cout<<root->left->data<<endl;
     cout<<root->right->data<<endl;
---
+++ threesum.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> threesum(vector<int> &nums){
+    vector<vector<int>>output;
+    set<vector<int>>s;
+    for(int i=0;i<nums.size();i++){
+        int target=0;
+        int j=i+1;
+        int k=nums.size()-1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum==target){
+                s.insert({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
+                
+            }
+            else if(sum<target){
+                j++;
+            }
+            else{
+                k--;
+            }
+        }
+    }
+    
+    for(auto p:s){
+        output.push_back(p);
+    }
+    return output;
+}
+
+int main() {
+	vector<int>vec={1,2,3,4,-5,-3,7,3,1};
+	vector<vector<int>>vec1;
+	vec1=threesum(vec);
+	
+      for(auto p:vec1){
+        for(auto a:p){
+            cout<<a<<" ";
+        }
+        cout<<endl;
+    }
+}
