c4c1ed82713937a73186bda95742c882d1f04a86
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-03-27 19:16:12 +0530

    tree

---
+++ binary tree implementation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class node{
+    public: int data;
+    node *left;
+    node *right;
+    node(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+int main(){
+    node *root=new node(10);
+    root->left=new node(20);
+    root->right=new node(30);
+    cout<<"preorder\n"
+    cout<<root->data<<endl;
+    cout<<root->left->data<<endl;
+    cout<<root->right->data<<endl;
+    
+}
